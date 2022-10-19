4a38f50cd0c5bd3024a1411ba332612cd7ce86cd
Joan Montoya <joan15montoya@gmail.com>
2022-10-18 22:29:39 -0500

    segundo commit

--- Login/src/app/Components/login/login.component.ts
+++ Login/src/app/Components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UsuariosService } from '../../Services/usuarios.service'
 import Swal from 'sweetalert2'
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +13,11 @@ export class LoginComponent implements OnInit {
   @Input() usuarios: any 
 
   Usuario = {
-    password: null,
-    email: null
+    password: "",
+    email: ""
   }
-
-  constructor(public UsuariosService: UsuariosService) { }
+  
+  constructor(public UsuariosService: UsuariosService) {}
 
   ngOnInit(){
     this.obtenerUsuarios();
@@ -33,30 +34,28 @@ export class LoginComponent implements OnInit {
   }
 
   log(){
-    for(let x = 0; x < this.usuarios.length; x++){
-      if(this.Usuario.email == this.usuarios[x].email && this.Usuario.password == this.usuarios[x].password){
-        console.log("si")
-        Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Credenciales correctas',
-          showConfirmButton: false,
-          timer: 1500
-        })
-        break;
-       
-      }else{
-        console.log("no")
-        Swal.fire({
-          position: 'center',
-          icon: 'error',
-          title: 'Credeciales incorrectas',
-          showConfirmButton: false,
-          timer: 1500
-        })
+    
+      for(let x = 0; x < this.usuarios.length; x++){
+        if(this.Usuario.email == this.usuarios[x].email && this.Usuario.password == this.usuarios[x].password){
+          console.log("si")
+          
+          break;
+         
+        }else{
+          console.log("no")
+          Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Credeciales incorrectas',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        }
       }
+      
+    }
+
     }
     
-  }
 
-}
+
